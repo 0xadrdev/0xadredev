9e7e08858802befee1fe97e7da8642f9d5ba2063
Adr-dev <adalegre.dev@gmail.com>
2023-06-26 17:58:01 +0200

    Added remove menu.

--- js/options.js
+++ js/options.js
@@ -3,6 +3,15 @@ import {addElement, setElement, insertElement} from "./main.js";
 const addButton = document.getElementById("add-btn");
 const setButton = document.getElementById("set-btn");
 const insertButton = document.getElementById("insert-btn");
+const removeButton = document.getElementById("remove-btn");
+const removeButtonIcon = document.getElementById("remove-btn-icon");
+const selectsContainer = document.getElementById("selects-container")
+const selectButtonIndex = document.getElementById("select-btn-index");
+const selectButtonData = document.getElementById("select-btn-data");
+const removeIndexInput = document.querySelector(".option-input-remove:nth-child(3)");
+const removeDataInput = document.querySelector(".option-input-remove:nth-child(4)");
+
+
 
 addButton.addEventListener("click", () => {
   let addContainer = addButton.parentElement; 
@@ -22,6 +31,45 @@ insertButton.addEventListener("click", () => {
   let insertIndexInput = insertContainer.querySelector("#index-input");
   let insertDataInput = insertContainer.querySelector("#data-input");
   insertElement(insertIndexInput.value, insertDataInput.value);
+});
+
+function showRemoveIndexInput() {
+  selectsContainer.style.setProperty("display", "none");
+  selectButtonData.classList.remove("show");
+  selectButtonIndex.classList.remove("show");
+  removeIndexInput.style.setProperty("display", "block");
+}
+
+function showRemoveDataInput() {
+  selectsContainer.style.setProperty("display", "none");
+  selectButtonData.classList.remove("show");
+  selectButtonIndex.classList.remove("show");
+  removeDataInput.style.setProperty("display", "block");
+}
+
+removeButtonIcon.addEventListener("click", e => {
+  e.stopPropagation();
+  selectButtonIndex.onclick = () => showRemoveIndexInput();
+  selectButtonData.onclick = () => showRemoveDataInput();
+  selectsContainer.style.setProperty("display", "block");
+  removeIndexInput.style.setProperty("display", "none");
+  removeDataInput.style.setProperty("display", "none");
+  if (selectButtonData.classList.contains("show")) {
+    selectButtonData.classList.remove("show");
+    selectButtonIndex.classList.remove("show");
+    removeButtonIcon.style.setProperty("transform", "rotate(-90deg)")
+  } else {
+    setTimeout(() => {
+      selectButtonData.classList.add("show");
+      selectButtonIndex.classList.add("show");
+      removeButtonIcon.style.setProperty("transform", "rotate(90deg)");
+    }, 10);
+  }
 })
+
+removeButton.addEventListener("click", e => {
+  // console.log("ENTRO")
+});
+
 
 
