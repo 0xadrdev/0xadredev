def9a6b5b06871d022acfedaabfce9ff6d4e869d
Adr-dev <adalegre.dev@gmail.com>
2023-10-26 00:19:30 +0200

    Check errors function.

--- js/main.js
+++ js/main.js
@@ -24,66 +24,54 @@ function removeError() {
 export function setError(errorText) {
   errorContainer.innerHTML = `<i class="fa-solid fa-circle-exclamation"></i> ${errorText}`;
   errorContainer.classList.add("show");
+  return true;
+}
+
+function checkErrors(type, str) {
+  if (linkedList.processStarted) return setError("Wait until adding process is finished. ");
+
+  if (type == "index" && str.trim() == "") return setError("Index must be a number. ");
+  if (type == "index" && (str < 0 || str >= linkedList.length())) return setError("Index out of bounds. ");
+
+  if (type == "data" && str.trim() == "") return setError("Data must be a number. ")
+
+  removeError();
+  return false; 
 }
 
 export async function addElement(data) {
-  if (linkedList.processStarted) return setError("Wait until adding process is finished. ")
-  if (data.trim() == "") return setError("Data must be a number. ");
-  removeError();
+  if (checkErrors("data", data)) return;
+
   let newNode = createNode(data); 
   await linkedList.add(newNode);
   removeError();
 }
 
 export async function setElement(index, data) {
-  if (linkedList.processStarted) return setError("Wait until setting process is finished. ")
-  if (index.trim() == "") {
-    return setError("Index must be a number. ");
-  } else if (index < 0 || index >= linkedList.length()) {
-    return setError("Index out of bounds. ");
-  } else if (data.trim() == ""){
-    return setError("Data must be a number. ");
-  }
-  removeError();
+  if (checkErrors("index", index) || checkErrors("data", data)) return;
+
   await linkedList.set(index, data);
   removeError();
 }
 
 export async function insertElement(index, data) {
-  if (linkedList.processStarted) return setError("Wait until inserting process is finished. ")
-  if (index.trim() == "") {
-    return setError("Index must be a number. ");
-  } else if (index < 0 || index >= linkedList.length()) {
-    return setError("Index out of bounds. ");
-  } else if (data.trim() == ""){
-    return setError("Data must be a number. ");
-  }
-  removeError();
+  if (checkErrors("index", index) || checkErrors("data", data)) return;
+
   let newNode = createNode(data);
   await linkedList.insert(index, newNode);
   removeError();
 }
 
 export async function removeElementAtIndex(index) {
-  if (linkedList.processStarted) return setError("Wait until removing process is finished. ")
-  if (index.trim() == "") {
-    return setError("Index must be a number. ");
-  } else if (index < 0 || index >= linkedList.length()) {
-    return setError("Index out of bounds. ");
-  } 
-  removeError();
+  if (checkErrors("index", index)) return;
+
   await linkedList.removeAtIndex(index);
   removeError();
 }
 
 export async function removeElementsWithData(data) {
-  if (linkedList.processStarted) return setError("Wait until removing process is finished. ")
-  if (data.trim() == "") {
-    return setError("Data must be a number. ");
-  } else if (linkedList.length() == 0) {
-    return setError("List is empty. ");
-  }
-  removeError();
+  if (checkErrors("data", data)) return;
+
   await linkedList.remove(data);
   removeError();
 }
